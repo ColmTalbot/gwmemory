1dc55e108ba0690866dc284515baa4ba915f35dd
Colm Talbot <colm.talbot@ligo.org>
2018-11-09 17:05:45 +1100

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='gwmemory',
-      version='0.1.0',
+      version='0.1.1',
       packages=['gwmemory'],
       package_dir={'gwmemory': 'gwmemory'},
       package_data={'gwmemory': ['data/gamma_coefficients*.dat', 'data/*WEB.dat']},
