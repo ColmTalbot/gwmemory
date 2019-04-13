18d0c73112b9ee3c559b291ae339cf88a755f580
Colm Talbot <colm.talbot@ligo.org>
2019-04-13 14:57:55 +1000

    version bump

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from distutils.core import setup
 
 setup(name='gwmemory',
-      version='0.1.1',
-      packages=['gwmemory'],
+      version='0.2.0',
+      packages=['gwmemory', 'gwmemory.waveforms'],
       package_dir={'gwmemory': 'gwmemory'},
       package_data={'gwmemory': ['data/gamma_coefficients*.dat', 'data/*WEB.dat']},
       )
