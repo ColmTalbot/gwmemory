02a928c507a39c26f1b0e368b2bb762874dd6be2
ColmTalbot <ColmTalbot@users.noreply.github.com>
2022-11-12 17:47:23 +0000

    deploy: d27f649c65a897e53e6522fccdaeece66a153caf

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev6+g8945c13',
+    VERSION: '1.0.1.dev9+gd27f649',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
