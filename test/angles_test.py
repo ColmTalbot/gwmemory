2645aa06526ae68c7540cadfd170223311b05ea1
Colm Talbot <talbotcolm@gmail.com>
2026-05-14 15:25:31 -0400

    MAINT: update some build steps (#27)
    * MAINT: update some build steps
    
    * dummy commit for CI
    
    * Bump versions for CI
    
    * Another attempt to fix the CI
    
    * Add gsl to test environment
    
    * Remove old python
    
    * Add pandoc to pages job

--- test/angles_test.py
+++ test/angles_test.py
@@ -1,23 +1,8 @@
 import numpy as np
-import pytest
 from sxs.waveforms import WaveformModes
 from sxs.waveforms.memory import Dinverse
 
 from gwmemory import angles
-
-
-@pytest.mark.parametrize("lm", [(2, 2), (2, -2), (3, 3), (3, 1)])
-def test_numeric_gamma_agrees_with_analytic(lm):
-    numeric = np.real(angles.gamma("22", f"{lm[0]}{lm[1]}"))
-    analytic = np.array(
-        [angles.analytic_gamma((2, 2), lm, ell) for ell in range(2, len(numeric) + 2)]
-    )
-    assert max(abs(numeric - analytic)) < 1e-4
-    numeric = np.real(angles.gamma(f"{lm[0]}{lm[1]}", "22"))
-    analytic = np.array(
-        [angles.analytic_gamma(lm, (2, 2), ell) for ell in range(2, len(numeric) + 2)]
-    )
-    assert max(abs(numeric - analytic)) < 1e-4
 
 
 def test_memory_factor_matches_sxs():
